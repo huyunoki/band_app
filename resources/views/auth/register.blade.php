7d3eb912d09be2b38aa5bf50de9ab3dba09297c1
huyunoki <k161616@kansai-u.ac.jp>
2024-09-01 09:36:32 +0900

    カレンダー機能完成（残りカレンダーごとにuser_idを割り振る）

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,9 @@
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
-
+            <x-primary-button class="ms-4">
+                <a href="{{ route('login') }}">ログイン画面はこちら</a>
+            </x-primary-button>
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
--- resources/views/calendar/calendar.blade.php
+++ resources/views/calendar/calendar.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>スケジュール画面</title>
         <!--<link rel="stylesheet" href="/../../css/index.css">-->
-        @vite(['resources/css/app.css', 'resources/js/calendar.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <x-app-layout>
