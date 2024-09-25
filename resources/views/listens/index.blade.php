653fbe2fed00eba955d02009af57bcdfa96fcb2a
huyunoki <k161616@kansai-u.ac.jp>
2024-09-25 20:50:41 +0900

    修正

--- resources/views/listens/index.blade.php
+++ resources/views/listens/index.blade.php
@@ -36,39 +36,39 @@
             <div class='paginate'>
                 {{ $uploads->links() }}
             </div>
-            <script>
-                const likedUploads = @json($likedUploads);
-
-                document.addEventListener('DOMContentLoaded', () => {
-                    document.querySelectorAll('.like-btn').forEach(likeBtn => {
-                        const postId = likeBtn.id;
-                        if (likedUploads.includes(parseInt(postId))) {
-                            likeBtn.classList.add('liked');
-                        }
-
-                        likeBtn.addEventListener('click', async (e) => {
-                            const clickedEl = e.target;
-                            clickedEl.classList.toggle('liked');
-                            const postId = e.target.id;
-
-                            try {
-                                const res = await fetch('/upload/like', {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                                    },
-                                    body: JSON.stringify({ upload_id: postId })
-                                });
-                                const data = await res.json();
-                                clickedEl.nextElementSibling.innerHTML = data.likesCount;
-                            } catch (error) {
-                                alert('処理が失敗しました。画面を再読み込みし、通信環境の良い場所で再度お試しください。');
-                            }
-                        });
-                    });
-                });
-            </script>
         </x-app-layout>
     </body>
+    <script>
+        const likedUploads = @json($likedUploads);
+
+        document.addEventListener('DOMContentLoaded', () => {
+            document.querySelectorAll('.like-btn').forEach(likeBtn => {
+                const postId = likeBtn.id;
+                if (likedUploads.includes(parseInt(postId))) {
+                    likeBtn.classList.add('liked');
+                }
+
+                likeBtn.addEventListener('click', async (e) => {
+                    const clickedEl = e.target;
+                    clickedEl.classList.toggle('liked');
+                    const postId = e.target.id;
+
+                    try {
+                        const res = await fetch('/upload/like', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                            },
+                            body: JSON.stringify({ upload_id: postId })
+                        });
+                        const data = await res.json();
+                        clickedEl.nextElementSibling.innerHTML = data.likesCount;
+                    } catch (error) {
+                        alert('処理が失敗しました。画面を再読み込みし、通信環境の良い場所で再度お試しください。');
+                    }
+                });
+            });
+        });
+    </script>
 </html>
