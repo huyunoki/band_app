e5a0a715398394835bf80dfaca18fab83b605799
huyunoki <k161616@kansai-u.ac.jp>
2024-09-26 19:27:18 +0900

    a

--- resources/views/listens/user.blade.php
+++ resources/views/listens/user.blade.php
@@ -11,22 +11,25 @@
     </head>
     <body>
         <x-app-layout>
-            <div class="social-icons">
-                <a href="{{ $line }}" target="_blank">
-                    <i class="fa-brands fa-line"></i>
-                </a>
-                @if($instagram)
-                    <a href="{{ $instagram }}" target="_blank">
-                        <i class="fa-brands fa-instagram"></i>
-                    </a>
-                @else
-                @endif
+            <div class="chat-button">
+                <a href="/chat/{{ $user->id }}">{{ $user->name }}とチャットする</a>
             </div>
-            <a href="/chat/{{ $user->id }}" class="chat-button">{{ $user->name }}とチャットする</a>
             <div class="user-content-container">
                 <div class="user-content">
                     <span>{{ $user->name }}</span>
-                    <h1>の音楽</h1>
+                    の音楽
+                </div>
+                <div class="social-icons">
+                    @if($line)
+                        <a href="{{ $line }}" target="_blank">
+                            <i class="fa-brands fa-line"></i>
+                        </a>
+                    @endif
+                    @if($instagram)
+                        <a href="{{ $instagram }}" target="_blank">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    @endif
                 </div>
             </div>
             <div class='listens'>
@@ -52,38 +55,38 @@
                 @endforeach
             </div>
             </x-app-layout>
-        <script>
-            const likedUploads = @json($likedUploads);
-
-            document.addEventListener('DOMContentLoaded', () => {
-                document.querySelectorAll('.like-btn').forEach(likeBtn => {
-                    const postId = likeBtn.id;
-                    if (likedUploads.includes(parseInt(postId))) {
-                        likeBtn.classList.add('liked');
-                    }
-
-                    likeBtn.addEventListener('click', async (e) => {
-                        const clickedEl = e.target;
-                        clickedEl.classList.toggle('liked');
-                        const postId = e.target.id;
-
-                        try {
-                            const res = await fetch('/upload/like', {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-                                },
-                                body: JSON.stringify({ upload_id: postId })
-                            });
-                            const data = await res.json();
-                            clickedEl.nextElementSibling.innerHTML = data.likesCount;
-                        } catch (error) {
-                            alert('処理が失敗しました。画面を再読み込みし、通信環境の良い場所で再度お試しください。');
-                        }
+        </body>
+    <script>
+    const likedUploads = @json($likedUploads);
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('.like-btn').forEach(likeBtn => {
+            const postId = likeBtn.id;
+            if (likedUploads.includes(parseInt(postId))) {
+                likeBtn.classList.add('liked');
+            }
+    
+            likeBtn.addEventListener('click', async (e) => {
+                const clickedEl = e.target;
+                clickedEl.classList.toggle('liked');
+                const postId = e.target.id;
+    
+                try {
+                    const res = await fetch('/upload/like', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                        },
+                        body: JSON.stringify({ upload_id: postId })
                     });
-                });
+                    const data = await res.json();
+                    clickedEl.nextElementSibling.innerHTML = data.likesCount;
+                } catch (error) {
+                    alert('処理が失敗しました。画面を再読み込みし、通信環境の良い場所で再度お試しください。');
+                }
             });
-        </script>
-    </body>
+        });
+    });
+    </script>
 </html>
