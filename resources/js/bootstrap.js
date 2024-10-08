b589fb562dca9cbc8aee65379403588a2a97eed2
huyunoki <k161616@kansai-u.ac.jp>
2024-10-08 15:04:15 +0900

    チャットページ訂正

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,8 +18,10 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 Pusher.logToConsole = true;
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
